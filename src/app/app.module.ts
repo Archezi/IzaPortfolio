23569269342af4d34f00026c3acbe23ce50a695a
archezi <luka.kaczmarek@gmail.com>
2020-03-18 13:57:12 +0000

    First upload of the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'navigation', component: NavigationComponent },
+  { path: 'content', component: ContentComponent },
+  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    ContentComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
